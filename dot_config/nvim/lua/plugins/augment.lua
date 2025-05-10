3ec1a4a6192d2666057e29520feef8a022a993c0
Dash Vallance <dash@intelics.com.au>
2025-05-10 21:44:06 +1000

    rm nvim.augment

--- dot_config/nvim/lua/plugins/augment.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "augmentcode/augment.vim",
-  config = function()
-    vim.keymap.set("n", "<leader>Q", ":Aug chat<CR>", { noremap = true, silent = true })
-    vim.g.augment_workspace_folders = { vim.fn.getcwd() }
-  end,
-}
