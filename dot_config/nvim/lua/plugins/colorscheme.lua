4a418f4352a2f23ef013a81661c58726c2920477
Dash Vallance <dash@intelics.com.au>
2025-06-06 21:08:13 +1000

    nvim tidy, add pywal colors

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+return {
+  "AlphaTechnolog/pywal.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('pywal').setup()
+    vim.cmd.colorscheme('pywal')
+  end,
+}
--- dot_config/nvim/lua/plugins/lazy.lua
+++ dot_config/nvim/lua/plugins/lazy.lua
@@ -1,14 +1,17 @@
 return {
-  { "atmosuwiryo/vim-winteriscoming" },
+  -- { "atmosuwiryo/vim-winteriscoming" },
 
-  {
-    "atmosuwiryo/vim-winteriscoming",
-    config = function()
-      vim.cmd("colorscheme WinterIsComing-dark-blue-color-theme")
-    end
-  },
+  -- {
+  --   "atmosuwiryo/vim-winteriscoming",
+  --   config = function()
+  --     vim.cmd("colorscheme WinterIsComing-dark-blue-color-theme")
+  --   end
+  -- },
   -- nixos doesn't support mason
   { "williamboman/mason.nvim",           enabled = false },
   { "williamboman/mason-lspconfig.nvim", enabled = false },
   { "jay-babu/mason-null-ls.nvim",       enabled = false },
+  { "windwp/nvim-autopairs",             enabled = false },
+  { "windwp/nvim-ts-autotag",            enabled = false },
+  { "catppuccin/nvim",                   name = "catppuccin", enabled = false },
 }
